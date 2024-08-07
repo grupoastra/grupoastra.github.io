e348f6b8b8b926a0562be377f28016592524ad32
sizuhe <simon.zuluagah@udea.edu.co>
2024-08-07 00:13:02 -0500

    Updated - Navbar active, Volta update, internal code

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -30,3 +30,21 @@ $(document).ready(function () {
             .appendTo("#slideshow");
     }, 3000);
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const currentPath = window.location.pathname;
+    const menuItem = document.querySelectorAll('#menu-item');
+
+    menuItem.forEach(item => {
+        if (item.getAttribute('href') === currentPath) {
+            item.classList.add('is-active');
+        }
+
+        item.addEventListener('click', function() {
+            menuItem.forEach(i => i.classList.remove('is-active'));
+            this.classList.add('is-active');
+        });
+    });
+});
